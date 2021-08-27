37a70c0886ca5577ed277d9621c16956ee20ba1f
kstobiecki <stobiecki.kamil@gmail.com>
2021-08-27 14:42:26 +0200

    task: add error handling, create error-message.enum.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,47 +1,95 @@
-import { Inject, Injectable, Logger } from '@nestjs/common';
+import {
+  ConflictException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { ItemRepository } from './item.repository';
 import { CreateItemDto, ItemDto } from './dto';
 import { plainToClass } from 'class-transformer';
 import { ItemEntity } from './entity/item.entity';
 import { QueryRunner } from 'typeorm/query-runner/QueryRunner';
+import { ErrorMessageEnum } from './enums/error-message.enum';
 
 @Injectable()
 export class AppService {
   constructor(@Inject('queryRunner') private readonly qr: QueryRunner) {}
 
   public async getItems(): Promise<ItemDto[]> {
-    Logger.debug({ message: '[getItems] Searching for items' });
-    const repo = this.qr.manager.getCustomRepository(ItemRepository);
-    const items = await repo.find();
-    Logger.debug({ message: '[getItems] Returning items', data: items });
-    return items;
+    try {
+      Logger.debug({ message: '[getItems] Searching for items' });
+      const repo = this.qr.manager.getCustomRepository(ItemRepository);
+      const items = await repo.find();
+      Logger.debug({ message: '[getItems] Returning items', data: items });
+      return items;
+    } catch (error: unknown) {
+      Logger.debug({ message: `[getItems - error] items not found` });
+      throw new NotFoundException({
+        statusCode: HttpStatus.NOT_FOUND,
+        error,
+        message: ErrorMessageEnum.ITEM_NOT_FOUND,
+      });
+    }
   }
 
   public async addItem(item: CreateItemDto): Promise<ItemDto> {
-    Logger.debug({ message: `[addItem] Adding an item ${item}` });
-    const repo = this.qr.manager.getCustomRepository(ItemRepository);
-    const savedItem = await repo.save(item);
+    try {
+      Logger.debug({ message: `[addItem] Adding an item ${item}` });
+      const repo = this.qr.manager.getCustomRepository(ItemRepository);
+      const savedItem = await repo.save(item);
 
-    Logger.debug({
-      message: '[addItem] Returning saved entity',
-      data: savedItem,
-    });
-    return plainToClass(ItemEntity, savedItem);
+      Logger.debug({
+        message: '[addItem] Returning saved entity',
+        data: savedItem,
+      });
+      return plainToClass(ItemEntity, savedItem);
+    } catch (error: unknown) {
+      Logger.debug({ message: `[addItem - error] item was not created` });
+      throw new ConflictException({
+        statusCode: HttpStatus.CONFLICT,
+        error,
+        message: ErrorMessageEnum.ITEM_NOT_CREATED,
+      });
+    }
   }
 
   public async deleteItem(itemId: string): Promise<void> {
-    Logger.debug({
-      message: `[deleteItem] Deleting an item with id ${itemId}`,
-    });
-    const repo = this.qr.manager.getCustomRepository(ItemRepository);
-    await repo.softDelete(itemId);
+    try {
+      Logger.debug({
+        message: `[deleteItem] Deleting an item with id ${itemId}`,
+      });
+      const repo = this.qr.manager.getCustomRepository(ItemRepository);
+      await repo.softDelete(itemId);
+    } catch (error: unknown) {
+      Logger.debug({
+        message: `[deleteItem - error] item with id ${itemId} was not deleted`,
+      });
+      throw new ConflictException({
+        statusCode: HttpStatus.CONFLICT,
+        error,
+        message: ErrorMessageEnum.ITEM_NOT_DELETED,
+      });
+    }
   }
 
   public async restoreItem(itemId: string): Promise<void> {
-    Logger.debug({
-      message: `[restoreItem] Restoring an item with id ${itemId}`,
-    });
-    const repo = this.qr.manager.getCustomRepository(ItemRepository);
-    await repo.restore(itemId);
+    try {
+      Logger.debug({
+        message: `[restoreItem] Restoring an item with id ${itemId}`,
+      });
+      const repo = this.qr.manager.getCustomRepository(ItemRepository);
+      await repo.restore(itemId);
+    } catch (error: unknown) {
+      Logger.debug({
+        message: `[restoreItem - error] item with id ${itemId} was not restored`,
+      });
+      throw new ConflictException({
+        statusCode: HttpStatus.CONFLICT,
+        error,
+        message: ErrorMessageEnum.ITEM_NOT_RESTORED,
+      });
+    }
   }
 }
---
+++ src/enums/error-message.enum.ts
@@ -0,0 +1,6 @@
+export enum ErrorMessageEnum {
+  ITEM_NOT_CREATED = 'item-not-created',
+  ITEM_NOT_FOUND = 'item-not-found',
+  ITEM_NOT_DELETED = 'item-not-deleted',
+  ITEM_NOT_RESTORED = 'item-not-restored',
+}
